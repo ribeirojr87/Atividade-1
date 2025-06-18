8bb8748a04c7761d3260f9ad4f7bfb4cc0bdd34f
Rômulo Ribeiro <120759927+ribeirojr87@users.noreply.github.com>
2025-06-17 22:30:18 -0300

    Update cadeirainfantil.py

--- cadeirainfantil.py
+++ cadeirainfantil.py
@@ -1,8 +1,8 @@
 #Calcular o preço total da cadeira infantil
 
-nome = "cadeira infantil"
-preco_unitario = float(input("Digite o preço do produto: ")) #float = converte texto em número decimal.
-quantidade = int(input("Digite a quantidade: ")) #int = converte texto em número inteiro.
+nome = input("Digite o nome do produto: ")
+preco_unitario = float(input("Digite o preço do produto: ")) 
+quantidade = int(input("Digite a quantidade: ")) 
 total = preco_unitario * quantidade
 
-print(f"O preço final é: R${total:.2f}")
+print(f"O preço do {nome} é: R${total:.2f}")
